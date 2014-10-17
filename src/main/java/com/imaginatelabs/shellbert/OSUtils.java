d39ba5c6ffce0db3cbc91a9887ad8ba8359f093f
Wayne Allan <wayne.allan@imaginatelabs.com>
2014-10-17 18:52:59 +1100

    Add OS detection for Solaris

--- src/main/java/com/imaginatelabs/shellbert/OSUtils.java
+++ src/main/java/com/imaginatelabs/shellbert/OSUtils.java
@@ -17,12 +17,14 @@ public final class OSUtils {
     public static OS detectOS() {
         if (cachedOS == null) {
             String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
-            if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+            if (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 ) {
+                cachedOS = com.imaginatelabs.shellbert.OS.Unix;
+            } else if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
                 cachedOS = com.imaginatelabs.shellbert.OS.MacOSX;
             } else if (OS.indexOf("win") >= 0) {
                 cachedOS = com.imaginatelabs.shellbert.OS.Windows;
-            } else if (OS.indexOf("nux") >= 0) {
-                cachedOS = com.imaginatelabs.shellbert.OS.Unix;
+            } else if (OS.indexOf("sunos") >= 0) {
+                cachedOS = com.imaginatelabs.shellbert.OS.Solaris;
             } else {
                 cachedOS = com.imaginatelabs.shellbert.OS.Other;
             }
